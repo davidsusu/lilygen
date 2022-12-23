efa1d615ca72670c499bfd052b34fccc46961fc9
Dávid Horváth <horvath@webarticum.hu>
2022-12-23 22:05:22 +0100

    Produce mp3 instead of wav

--- template/ctl.sh
+++ template/ctl.sh
@@ -19,6 +19,7 @@ elif [ "$COMMAND" = 'install' ]; then
     sudo apt-get install frescobaldi &&
     sudo apt-get install okular &&
     sudo apt-get install bash-completion &&
+    sudo apt-get install ffmpeg -y &&
     echo 'Installation completed.';
 elif [ "$COMMAND" = 'edit' ]; then
     frescobaldi "${PROJECT_DIR}/score.ly" 2>&1 > /dev/null &
@@ -26,8 +27,10 @@ elif [ "$COMMAND" = 'build' ]; then
     ls -A1 "${PROJECT_DIR}/out" | egrep -v '.gitignore' | xargs -i rm "${PROJECT_DIR}/out/{}"
     lilypond "${PROJECT_DIR}/score.ly"
     musescore -o "${PROJECT_DIR}/out/score.wav" "${PROJECT_DIR}/out/score.midi"
+    ffmpeg -i "${PROJECT_DIR}/out/score.wav" -acodec mp3 "${PROJECT_DIR}/out/score.mp3" &&
+    rm "${PROJECT_DIR}/out/score.wav"
 elif [ "$COMMAND" = 'play' ]; then
-    vlc "${PROJECT_DIR}/out/score.wav" 2>&1 > /dev/null &
+    vlc "${PROJECT_DIR}/out/score.mp3" 2>&1 > /dev/null &
 elif [ "$COMMAND" = 'display' ]; then
     okular "${PROJECT_DIR}/out/score.pdf" 2>&1 > /dev/null &
 elif [ -z "$COMMAND" ]; then
